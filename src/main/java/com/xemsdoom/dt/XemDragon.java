aa18b8687d75363f13541c2894a3187cd169b2b6
Matt Richards <mrichards@gmail.com>
2013-03-30 20:39:25 -0500

    Revert "Merge pull request #2 from davejavu/master"
    This reverts commit 4dfc117d2ae6eeed22d1e93594caf82e0be0dd00, reversing
    changes made to 209e0afc99c9458c594ee41158d37a57b1b224ff.

--- src/main/java/com/xemsdoom/dt/XemDragon.java
+++ src/main/java/com/xemsdoom/dt/XemDragon.java
@@ -1,7 +1,7 @@
 package com.xemsdoom.dt;
 
-import net.minecraft.server.v1_4_R1.EntityEnderDragon;
-import net.minecraft.server.v1_4_R1.World;
+import net.minecraft.server.v1_5_R2.EntityEnderDragon;
+import net.minecraft.server.v1_5_R2.World;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
